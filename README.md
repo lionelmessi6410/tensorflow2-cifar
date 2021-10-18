4cc8e3a77ce8e75032a0f2c7542150355fd1bdd7
Jimmy Yuan <lionelmessi6410@gmail.com>
2021-10-19 01:28:49 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Training CIFAR-10 with TensorFlow2(TF2)
 I'm playing with [TensorFlow](https://www.tensorflow.org/) on the [CIFAR-10](https://www.cs.toronto.edu/~kriz/cifar.html) dataset.
 
+## Architectures
+- [LeNet](https://ieeexplore.ieee.org/abstract/document/726791)
+- [AlexNet](https://papers.nips.cc/paper/2012/hash/c399862d3b9d6b76c8436e924a68c45b-Abstract.html)
+- [VGG](https://arxiv.org/abs/1409.1556) [11, 13, 16, 19]
+- [ResNet](https://arxiv.org/abs/1512.03385) [18, 34, 50, 101, 152]
+- [DenseNet](https://arxiv.org/abs/1608.06993) [121, 169, 201]
+- [PreActResNet](https://arxiv.org/abs/1603.05027) [18, 34, 50, 101, 152]
+- [SENet](https://arxiv.org/abs/1709.01507)
+- [SEResNet](https://arxiv.org/abs/1709.01507) [18, 34, 50, 101, 152]
+- [SEPreActResNet](https://arxiv.org/abs/1709.01507) [18, 34, 50, 101, 152]
+- [MobileNet](https://arxiv.org/abs/1704.04861) 
+- [MobileNetV2](https://arxiv.org/abs/1801.04381)
+
 ## Prerequisites
 - Python 3.8+
 - TensorFlow 2.4.0+
@@ -8,35 +21,45 @@ I'm playing with [TensorFlow](https://www.tensorflow.org/) on the [CIFAR-10](htt
 ## Training
 Start training with: 
 ```python
-python main.py
+python train.py --model resnet18
 ```
 
 You can manually resume the training with: 
 ```python
-python main.py --resume
+python train.py --model resnet18 --resume
 ```
 
 ## Accuracy
-| Model             | Acc.        | Param.        |
-| ----------------- | ----------- | ------------: |
-| [VGG11](https://arxiv.org/abs/1409.1556)              | 92.61% | 9.2M |
-| [VGG13](https://arxiv.org/abs/1409.1556)              | 94.31% | 9.4M |
-| [VGG16](https://arxiv.org/abs/1409.1556)              | 94.27% | 14.7M |
-| [VGG19](https://arxiv.org/abs/1409.1556)              | 93.65% | 20.1M |
-| [ResNet18](https://arxiv.org/abs/1512.03385)          | 95.37% | 11.2M |
-| [ResNet34](https://arxiv.org/abs/1512.03385)          | 95.48% | 21.3M |
-| [ResNet50](https://arxiv.org/abs/1512.03385)          | 95.41% | 23.6M |
-| [ResNet101](https://arxiv.org/abs/1512.03385)         | 95.44% | 42.6M |
-| [ResNet152](https://arxiv.org/abs/1512.03385)         | 95.29% | 58.3M |
-| [MobileNet](https://arxiv.org/abs/1704.04861)         | 92.34% | 3.2M |
-| [MobileNetV2](https://arxiv.org/abs/1801.04381)       |  |  |
-<!-- | [RegNetX_200MF](https://arxiv.org/abs/2003.13678)     | 94.24%      |
-| [RegNetY_400MF](https://arxiv.org/abs/2003.13678)     | 94.29%      |
-| [MobileNetV2](https://arxiv.org/abs/1801.04381)       | 94.43%      |
-| [ResNeXt29(32x4d)](https://arxiv.org/abs/1611.05431)  | 94.73%      |
-| [ResNeXt29(2x64d)](https://arxiv.org/abs/1611.05431)  | 94.82%      |
-| [SimpleDLA](https://arxiv.org/abs/1707.064)           | 94.89%      |
-| [DenseNet121](https://arxiv.org/abs/1608.06993)       | 95.04%      |
-| [PreActResNet18](https://arxiv.org/abs/1603.05027)    | 95.11%      |
-| [DPN92](https://arxiv.org/abs/1707.01629)             | 95.16%      |
-| [DLA](https://arxiv.org/pdf/1707.06484.pdf)           | 95.47%      | -->
+| Model             | Acc.   | Param. |
+| ----------------- | ------ | -----: |
+| LeNet             | 67.85% |  0.06M |
+| AlexNet           | 78.81% |  21.6M |
+| VGG11             | 92.61% |   9.2M |
+| VGG13             | 94.31% |   9.4M |
+| VGG16             | 94.27% |  14.7M |
+| VGG19             | 93.65% |  20.1M |
+| ResNet18          | 95.37% |  11.2M |
+| ResNet34          | 95.48% |  21.3M |
+| ResNet50          | 95.41% |  23.6M |
+| ResNet101         | 95.44% |  42.6M |
+| ResNet152         | 95.29% |  58.3M |
+| DenseNet121       | 95.37% |   7.0M |
+| DenseNet169       | 95.10% |  12.7M |
+| DenseNet201       | 94.79% |  18.3M |
+| PreActResNet18    | % |  11.2M |
+| PreActResNet34    | % |  21.3M |
+| PreActResNet50    | % |  23.6M |
+| PreActResNet101   | % |  42.6M |
+| PreActResNet152   | % |  58.3M |
+| SEResNet18        | 95.44% |  11.3M |
+| SEResNet34        | 95.30% |  21.5M |
+| SEResNet50        | 95.76% |  26.1M |
+| SEResNet101       | 95.40% |  47.3M |
+| SEResNet152       | % |  64.9M |
+| SEPreActResNet18  | % |  11.3M |
+| SEPreActResNet34  | % |  21.5M |
+| SEPreActResNet50  | % |  26.1M |
+| SEPreActResNet101  | % |  47.3M |
+| SEPreActResNet152  | % |  64.9M |
+| MobileNet         | 92.34% |   3.2M |
+| MobileNetV2       | 94.03% |   2.3M |
---
+++ main.py
@@ -0,0 +1,176 @@
+"""Train CIFAR-10 with TensorFlow2.0."""
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import argparse
+from tqdm import tqdm
+
+from models import *
+from utils import *
+
+parser = argparse.ArgumentParser(description='TensorFlow2.0 CIFAR-10 Training')
+parser.add_argument('--model', required=True, type=str, help='model type')
+parser.add_argument('--lr', default=1e-1, type=float, help='learning rate')
+parser.add_argument('--batch_size', default=128, type=int, help='batch size')
+parser.add_argument('--epoch', default=200, type=int, help='number of training epoch')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--gpu', default=0, type=int, help='specify which gpu to be used')
+args = parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+
+class Model():
+    def __init__(self, model_type, decay_steps, num_classes=10):
+        if 'lenet' in model_type:
+            self.model = LeNet(num_classes)
+        elif 'alexnet' in model_type:
+            self.model = AlexNet(num_classes)
+        elif 'vgg' in model_type:
+            self.model = VGG(model_type, num_classes)
+        elif 'resnet' in model_type:
+            if 'sepreact' in model_type:
+                self.model = SEPreActResNet(model_type, num_classes)
+            elif 'se' in model_type:
+                self.model = SEResNet(model_type, num_classes)
+            else:
+                self.model = ResNet(model_type, num_classes)
+        elif 'densenet' in model_type:
+            self.model = DenseNet(model_type, num_classes)
+        elif 'mobilenet' in model_type:
+            if 'v2' not in model_type:
+                self.model = MobileNet(num_classes)
+            else:
+                self.model = MobileNetV2(num_classes)
+        else:
+            sys.exit(ValueError("{:s} is currently not supported.".format(model_type)))
+        
+        self.loss_object = tf.keras.losses.CategoricalCrossentropy()
+        learning_rate_fn = tf.keras.experimental.CosineDecay(args.lr, decay_steps=decay_steps)
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate_fn, momentum=0.9)
+        self.weight_decay = 5e-4
+        
+        self.train_loss = tf.keras.metrics.Mean(name='train_loss')
+        self.train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+        self.test_loss = tf.keras.metrics.Mean(name='test_loss')
+        self.test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+        
+    @tf.function
+    def train_step(self, images, labels):
+        with tf.GradientTape() as tape:
+            predictions = self.model(images, training=True)
+            # Cross-entropy loss
+            ce_loss = self.loss_object(labels, predictions)
+            # L2 loss(weight decay)
+            l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in self.model.trainable_variables])
+            loss = ce_loss + l2_loss*self.weight_decay
+            
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        
+        self.train_loss(loss)
+        self.train_accuracy(labels, predictions)
+
+    @tf.function
+    def test_step(self, images, labels):
+        predictions = self.model(images, training=False)
+        t_loss = self.loss_object(labels, predictions)
+        
+        self.test_loss(t_loss)
+        self.test_accuracy(labels, predictions)
+        
+    def train(self, train_ds, test_ds, epoch):
+        best_acc = tf.Variable(0.0)
+        curr_epoch = tf.Variable(0)  # start from epoch 0 or last checkpoint epoch
+        ckpt_path = './checkpoints/{:s}/'.format(args.model)
+        ckpt = tf.train.Checkpoint(curr_epoch=curr_epoch, best_acc=best_acc,
+                                   optimizer=self.optimizer, model=self.model)
+        manager = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=1)
+        
+        if args.resume:
+            # Load checkpoint.
+            print('==> Resuming from checkpoint...')
+            assert os.path.isdir(ckpt_path), 'Error: no checkpoint directory found!'
+
+            # Restore the weights
+            ckpt.restore(manager.latest_checkpoint)
+        
+        for e in tqdm(range(int(curr_epoch), epoch)):
+            # Reset the metrics at the start of the next epoch
+            self.train_loss.reset_states()
+            self.train_accuracy.reset_states()
+            self.test_loss.reset_states()
+            self.test_accuracy.reset_states()
+
+            for images, labels in train_ds:
+                self.train_step(images, labels)
+                
+            for images, labels in test_ds:
+                self.test_step(images, labels)
+
+            template = 'Epoch {:0}, Loss: {:.4f}, Accuracy: {:.2f}%, Test Loss: {:.4f}, Test Accuracy: {:.2f}%'
+            print (template.format(e+1,
+                                   self.train_loss.result(),
+                                   self.train_accuracy.result()*100,
+                                   self.test_loss.result(),
+                                   self.test_accuracy.result()*100))
+            
+            # Save checkpoint
+            if self.test_accuracy.result() > best_acc:
+                print('Saving...')
+                if not os.path.isdir('./checkpoints/'):
+                    os.mkdir('./checkpoints/')
+                if not os.path.isdir(ckpt_path):
+                    os.mkdir(ckpt_path)
+                best_acc.assign(self.test_accuracy.result())
+                curr_epoch.assign(e+1)
+                manager.save()
+    
+    def predict(self, pred_ds, best):
+        if best:
+            best_acc = tf.Variable(0.0)
+            curr_epoch = tf.Variable(0)  # start from epoch 0 or last checkpoint epoch
+            ckpt_path = './checkpoints/{:s}/'.format(args.model)
+            ckpt = tf.train.Checkpoint(curr_epoch=curr_epoch, best_acc=best_acc,
+                                       optimizer=self.optimizer, model=self.model)
+            manager = tf.train.CheckpointManager(ckpt, ckpt_path, max_to_keep=1)
+            
+            # Load checkpoint
+            print('==> Resuming from checkpoint...')
+            assert os.path.isdir(ckpt_path), 'Error: no checkpoint directory found!'
+            ckpt.restore(manager.latest_checkpoint)
+        
+        self.test_accuracy.reset_states()
+        for images, labels in pred_ds:
+            self.test_step(images, labels)
+        print ('Prediction Accuracy: {:.2f}%'.format(self.test_accuracy.result()*100))
+
+def main():
+    # Data
+    print('==> Preparing data...')
+    train_images, train_labels, test_images, test_labels = get_dataset()
+    mean, std = get_mean_and_std(train_images)
+    train_images = normalize(train_images, mean, std)
+    test_images = normalize(test_images, mean, std)
+
+    train_ds = dataset_generator(train_images, train_labels, args.batch_size)
+    test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).\
+            batch(args.batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+    class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                   'dog', 'frog', 'horse', 'ship', 'truck']
+    decay_steps = int(args.epoch*len(train_images)/args.batch_size)
+    
+    # Train
+    print('==> Building model...')
+    model = Model(args.model, decay_steps)
+    model.train(train_ds, test_ds, args.epoch)
+    
+    # Evaluate
+    model.predict(test_ds, best=True)
+
+if __name__ == "__main__":
+    main()
